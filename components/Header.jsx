21122f6ab8a2e0c07d16a3c0d93fcbc7b166fe5a
FabioMorrone <morronefabio04@gmail.com>
2025-03-20 16:51:34 +0100

    add Header component with navigation and update HTML title and styles

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,23 @@
 export default function header() {
     return (
-        <header  >
-
-
-
-        </header>
+        <header>
+            <div className="logo">
+                <img src="/favicon.ico" alt="" />
+            </div>
+            <nav>
+                <ul className="menu">
+                    <li><a href="">CHARACTERS</a></li>
+                    <li><a href="">COMICS</a></li>
+                    <li><a href="">MOVIES</a></li>
+                    <li><a href="">TV</a></li>
+                    <li><a href="">GAMES</a></li>
+                    <li><a href="">COLLECTIBLES</a></li>
+                    <li><a href="">VIDEOS</a></li>
+                    <li><a href="">FANS</a></li>
+                    <li><a href="">NEWS</a></li>
+                    <li><a href="">SHOP</a></li>
+                </ul>
+            </nav>
+        </header >
     )
 }
