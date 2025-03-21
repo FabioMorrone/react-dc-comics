b179f1d82b0de8fdaa3b5badba7868495271131c
FabioMorrone <morronefabio04@gmail.com>
2025-03-21 12:54:18 +0100

    refactor: restructure Header and Main components, update styles, and add Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Header from '../components/Header'
-import Main from '../components/Main'
-import Footer from '../components/Footer'
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
 
 
 function App() {
---
+++ src/components/Main.jsx
@@ -0,0 +1,28 @@
+export default function main() {
+    return (
+        <main>
+            <section className="container">
+                <div className="row-1">
+                    <div className="title">
+                        <h1>Content goes here</h1>
+                    </div>
+                </div>
+                <div className="row-2">
+                    <div className="icon">
+                        <img src="/buy-comics-digital-comics.png" alt="" />
+                        <span>DIGITAL COMICS</span>
+                        <img src="/buy-comics-merchandise.png" alt="" />
+                        <span>DC MERCHANDISE</span>
+                        <img src="/buy-comics-subscriptions.png" alt="" />
+                        <span>SUBSCRIPTION</span>
+                        <img src="/buy-comics-shop-locator.png" alt="" />
+                        <span>COMIC SHOP LOCATOR</span>
+                        <img src="/buy-dc-power-visa.svg" alt="" />
+                        <span>DC POWER VISA</span>
+
+                    </div>
+                </div>
+            </section>
+        </main>
+    )
+}
