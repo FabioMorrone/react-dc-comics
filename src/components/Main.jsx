1009c2e2a0f6919b385f72a35fdab2188eab4eb6
FabioMorrone <morronefabio04@gmail.com>
2025-03-21 16:15:39 +0100

    refactor: enhance Main component layout with info-icon class and update CSS for improved styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -17,19 +17,19 @@ export default function main() {
                                 <img src="/buy-comics-digital-comics.png" alt="" />
                                 <span>DIGITAL COMICS</span>
                             </li>
-                            <li >
+                            <li className="info-icon" >
                                 <img src="/buy-comics-merchandise.png" alt="" />
                                 <span>DC MERCHANDISE</span>
                             </li>
-                            <li>
+                            <li className="info-icon">
                                 <img src="/buy-comics-subscriptions.png" alt="" />
                                 <span>SUBSCRIPTION</span>
                             </li>
-                            <li>
+                            <li className="info-icon">
                                 <img src="/buy-comics-shop-locator.png" alt="" />
                                 <span>COMIC SHOP LOCATOR</span>
                             </li>
-                            <li>
+                            <li className="info-icon">
                                 <img src="/buy-dc-power-visa.svg" alt="" />
                                 <span>DC POWER VISA</span>
                             </li>
