f61b1f55f17db25f07df237fe0a3d0840ddc2b28
FabioMorrone <morronefabio04@gmail.com>
2025-03-24 12:36:13 +0100

    refactor: simplify DcList component by mapping over comics data to render Card components

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,6 @@ export default function Card({ thumb, series }) {
 
     return (
         <div className="col">
-
             <div className='Card'>
                 <img src={thumb} alt={series} />
                 <div>{series}</div>
