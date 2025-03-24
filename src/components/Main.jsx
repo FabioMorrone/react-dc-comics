e7bd38accba22dac5b8a61da5a2be8e560e3430e
FabioMorrone <morronefabio04@gmail.com>
2025-03-24 08:21:11 +0100

    feat: add Card and DcList components for displaying comic series with thumbnails

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,40 +1,37 @@
-export default function main() {
+import DcList from "./DcList";
+
+export default function Main() {
     return (
         <main>
             <section className="banner_up">
-
             </section>
-            <section className="jumbotron">
-                <div className="container d-flex">
-
-                </div>
-            </section>
+            <DcList />
             <section className="banner">
                 <div className="container d-flex">
-                    <div className="icon">
-                        <ul className="unstile-list">
-                            <li className="info-icon">
-                                <img src="/buy-comics-digital-comics.png" alt="" />
-                                <span>DIGITAL COMICS</span>
-                            </li>
-                            <li className="info-icon" >
-                                <img src="/buy-comics-merchandise.png" alt="" />
-                                <span>DC MERCHANDISE</span>
-                            </li>
-                            <li className="info-icon">
-                                <img src="/buy-comics-subscriptions.png" alt="" />
-                                <span>SUBSCRIPTION</span>
-                            </li>
-                            <li className="info-icon">
-                                <img src="/buy-comics-shop-locator.png" alt="" />
-                                <span>COMIC SHOP LOCATOR</span>
-                            </li>
-                            <li className="info-icon">
-                                <img src="/buy-dc-power-visa.svg" alt="" />
-                                <span>DC POWER VISA</span>
-                            </li>
-                        </ul>
-                    </div>
+
+                    <ul className="unstile-list">
+                        <li className="info-icon">
+                            <img src="/buy-comics-digital-comics.png" alt="" />
+                            <span>DIGITAL COMICS</span>
+                        </li>
+                        <li className="info-icon" >
+                            <img src="/buy-comics-merchandise.png" alt="" />
+                            <span>DC MERCHANDISE</span>
+                        </li>
+                        <li className="info-icon">
+                            <img src="/buy-comics-subscriptions.png" alt="" />
+                            <span>SUBSCRIPTION</span>
+                        </li>
+                        <li className="info-icon">
+                            <img src="/buy-comics-shop-locator.png" alt="" />
+                            <span>COMIC SHOP LOCATOR</span>
+                        </li>
+                        <li className="info-icon">
+                            <img src="/buy-dc-power-visa.svg" alt="" />
+                            <span>DC POWER VISA</span>
+                        </li>
+                    </ul>
+
                 </div>
             </section>
         </main>
